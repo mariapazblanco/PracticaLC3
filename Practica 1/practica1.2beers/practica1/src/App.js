ce663f0d5488b6d921ce6700b5828f516ee1174e
mariapazblanco <pazblanco01@gmail.com>
2023-09-12 18:36:28 -0300

    update practice 1

---
+++ Practica 1/practica1.2beers/practica1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Beers from './components/beers/Beers';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Beers />
+    </div>
+  );
+}
+
+export default App;
